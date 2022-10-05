9bd720aa7680cb9b9e4ffe5c41c287c5fc4dabd6
Lucas Colombo <lucas.c.hurpia@gmail.com>
2022-10-05 10:07:23 -0700

    Updated router for better work in gitHub pages

--- scripts/routerSpa.js
+++ scripts/routerSpa.js
@@ -1,43 +1,35 @@
-//Encountered problems with setting up a SPA router that works on GitHub, the following code works in a live server but not when using github pages
-//For now, this code is being used for the routing
-//the router.js script and the Pages folder are not being used at the current moment
-//will update it soon
-
 const mapDisplay = document.getElementById('mapDisplay');
 const rootElement = document.getElementById('mainArea');
 
-//this section will be updated soon, for now this works just fine
-const login = `<div>this is the login page</div>`
-const startScreen = `<div>this is the startScreen page</div>`
-const register = `<div>this is the register page</div>`
-const survey = `<div>this is the survey page</div>`
-const profile = `<div>this is the profile page</div>`
-const badges = `<div>this is the badges page</div>`
-const challenges = `<div>this is the challenges page</div>`
-
 const pageIndex = [
-    { id: 'mainPage', innerhtml: ''},
-    { id: 'startScreen', innerhtml: startScreen},
-    { id: 'login', innerhtml: login},
-    { id: 'register', innerhtml: register},
-    { id: 'survey', innerhtml: survey},
-    { id: 'profile', innerhtml: profile},
-    { id: 'badges', innerhtml: badges},
-    { id: 'challenges', innerhtml: challenges},
+    { id: 'mainPage', htmlPath: '' },
+    { id: 'startScreen', htmlPath: '/Pages/startScreen.html' },
+    { id: 'login', htmlPath: '/Pages/login.html' },
+    { id: 'register', htmlPath: '/Pages/register.html' },
+    { id: 'survey', htmlPath: '/Pages/survey.html' },
+    { id: 'profile', htmlPath: '/Pages/profile.html' },
+    { id: 'badges', htmlPath: '/Pages/badges.html' },
+    { id: 'challenges', htmlPath: '/Pages/challenges.html' },
 ]
 
-const routerSPA = (id) =>{
+async function routerSPA(id) {
     for (let page of pageIndex) {
-        if(id=='mainPage'){ //the map is always loaded in the background, this reduces loading times when going back to the main page
+        if (id == 'mainPage') { //the map is always loaded in the background, this reduces loading times when going back to the main page
             mapDisplay.style.display = 'block'
             rootElement.style.display = 'none'
-            break;
+            return;
         }
-        else if(page.id==id){
-            mapDisplay.style.display = 'none'
-            rootElement.style.display = 'block'
-            rootElement.innerHTML = page.innerhtml
-            break;
+        else if (page.id == id) {
+            try {
+                mapDisplay.style.display = 'none'
+                rootElement.style.display = 'block'
+                const response = await fetch(page.htmlPath);
+                const txt = await response.text();
+                rootElement.innerHTML = txt;
+                return;
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 }
